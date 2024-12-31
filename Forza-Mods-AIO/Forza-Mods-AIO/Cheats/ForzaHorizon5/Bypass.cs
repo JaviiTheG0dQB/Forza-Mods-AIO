c62fc96174b872009a8b090caf8c2cf7a0121f02
merika <126014478+szaaamerik@users.noreply.github.com>
2024-12-31 14:33:10 +0100

    add new bypass - 1/3 bypasses wasted away

--- Forza-Mods-AIO/Forza-Mods-AIO/Cheats/ForzaHorizon5/Bypass.cs
+++ Forza-Mods-AIO/Forza-Mods-AIO/Cheats/ForzaHorizon5/Bypass.cs
@@ -10,6 +10,9 @@ public class Bypass : CheatsUtilities, ICheatsBase
     public readonly DebugSession BypassDebug = new("Bypass", []);
 
     public UIntPtr CallAddress;
+    public UIntPtr XxhCheck;
+    public UIntPtr OrigXxhCheck;
+    public UIntPtr Ret;
     private bool _scanning;
     private Timer _antiCheatTimer = null!;
     
@@ -29,13 +32,27 @@ public class Bypass : CheatsUtilities, ICheatsBase
         _scanning = true;
         CallAddress = 0;
 
-        const string sig = "49 8B ? FF 50 ? 48 8D ? ? ? ? ? FF 15 ? ? ? ? 8B 8D";
-        CallAddress = await SmartAobScan(sig) + 3;
+        Ret = await SmartAobScan("C3");
+        if (Ret == 0)
+        {
+            ShowError("Bypass", "C3");
+            return;
+        }
+        
+        const string sig = "48 8B D9 48 8D 05 ? ? ? ? 48 89 01 E8 ? ? ? ? 48 8B CB 48 83 C4 20 5B E9";
+        CallAddress = await SmartAobScan(sig);
 
         if (CallAddress > 3)
         {
+            UIntPtr pXxhCheckPfns = CallAddress + 0x3;
+            int pPfnRelative = GetInstance().ReadMemory<int>(pXxhCheckPfns + 0x3);
+            UIntPtr xxhCheckPfns = (UIntPtr)(pPfnRelative + (IntPtr)pXxhCheckPfns + 0x7);
+            XxhCheck = xxhCheckPfns + 0x30;
+            OrigXxhCheck = GetInstance().ReadMemory<UIntPtr>(XxhCheck);
+
+            
             _antiCheatTimer = new Timer();
-            _antiCheatTimer.Interval = 25000;
+            _antiCheatTimer.Interval = 10_000;
             _antiCheatTimer.Elapsed += async (_, _) =>
             {
                 var mem = GetInstance();
@@ -43,16 +60,16 @@ public class Bypass : CheatsUtilities, ICheatsBase
                 {
                     ((IRevertBase)pair.Value).Revert();
                 }
-                mem.WriteArrayMemory(CallAddress, new byte[] { 0xFF, 0x50, 0x30 });
-                await Task.Delay(500);
-                mem.WriteArrayMemory(CallAddress, new byte[] { 0x90, 0x90, 0x90 });
+                mem.WriteMemory(XxhCheck, OrigXxhCheck);
+                await Task.Delay(1_000);
+                mem.WriteMemory(XxhCheck, Ret);
                 foreach (var pair in CachedInstances.Where(kv => typeof(IRevertBase).IsAssignableFrom(kv.Key)))
                 {
                     ((IRevertBase)pair.Value).Continue();
                 }
             };
         
-            GetInstance().WriteArrayMemory(CallAddress, new byte[] { 0x90, 0x90, 0x90 });
+            GetInstance().WriteMemory(XxhCheck, Ret);
             _antiCheatTimer.Start();
             _scanning = false;
             return;
@@ -65,9 +82,9 @@ public class Bypass : CheatsUtilities, ICheatsBase
     public void Cleanup()
     {
         var mem = GetInstance();
-        if (CallAddress <= 3) return;
+        if (XxhCheck <= 3) return;
         _antiCheatTimer.Stop();
-        mem.WriteArrayMemory(CallAddress, new byte[] { 0xFF, 0x50, 0x30 });
+        mem.WriteMemory(XxhCheck, OrigXxhCheck);
     }
 
     public void Reset()
